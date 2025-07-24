5c841e30737152b0c485af42098c0ac8d75ae6dd
sathiyamoorthybot <sathiyamoorthyjayarajan@gmail.com>
2025-07-24 22:12:29 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,13 @@
 import { Link } from "react-router-dom";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import AuthButtons from "@/components/AuthButtons";
-
 const Header = () => {
-  return (
-    <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-sm bg-white/10">
+  return <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-sm bg-white/10">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex items-center">
-            <Link to="/">
-              <img
-                src="/lovable-uploads/446dd634-c297-4975-bb69-8470fc0b04ce.png"
-                alt="Western Ghats X"
-                className="h-14 w-auto"
-              />
-            </Link>
+            <img src="/lovable-uploads/446dd634-c297-4975-bb69-8470fc0b04ce.png" alt="Western Ghats X" className="h-14 w-auto" />
           </div>
 
           {/* Navigation */}
@@ -40,9 +24,7 @@ const Header = () => {
                 </Link>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <Link to="/kattanji-marathon" className={navigationMenuTriggerStyle()}>
-                  Kattanji Hills Marathon
-                </Link>
+                <Link to="/kattanji-marathon" className={navigationMenuTriggerStyle()}>Kattanji Hills Marathon</Link>
               </NavigationMenuItem>
               <NavigationMenuItem>
                 <Link to="/contact" className={navigationMenuTriggerStyle()}>
@@ -56,8 +38,6 @@ const Header = () => {
           <AuthButtons />
         </div>
       </div>
-    </header>
-  );
+    </header>;
 };
-
 export default Header;
