2ef9b0876608792bcdce34bb7478a49ee23726b5
sathiyamoorthybot <sathiyamoorthyjayarajan@gmail.com>
2025-08-06 11:30:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default {
-  base: './',
-};
-
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
