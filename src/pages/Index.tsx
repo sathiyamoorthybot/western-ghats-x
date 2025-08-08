8c2ee18f9e1a779e8ff6fd9151fabf3d52773abf
sathiyamoorthybot <sathiyamoorthyjayarajan@gmail.com>
2025-08-09 01:20:57 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,7 +7,7 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Header />
-      <div className="pt-20 md:pt-16">
+      <div className="pt-16 md:pt-10">
 
         <HeroSlider className="pb-10" />
         <WesternGhatsXIntro />
