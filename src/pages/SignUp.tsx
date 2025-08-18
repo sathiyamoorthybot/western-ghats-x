30883f55924b5eafe2baba455fc260374db523aa
sathiyamoorthybot <sathiyamoorthyjayarajan@gmail.com>
2025-08-18 15:06:49 +0530

    Update SignUp.tsx

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,14 +1,27 @@
 import AuthForm from "@/components/AuthForm";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import GoogleSignInButton from "@/components/GoogleSignInButton";
 
 const SignUp = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="flex-1 pt-2">
+      <div className="flex-1 pt-2 flex flex-col items-center">
+        {/* Existing signup form */}
         <AuthForm type="signup" />
+
+        {/* Divider */}
+        <div className="my-6 flex items-center w-full max-w-sm">
+          <div className="flex-grow border-t border-gray-300"></div>
+          <span className="mx-3 text-gray-500 text-sm">OR</span>
+          <div className="flex-grow border-t border-gray-300"></div>
+        </div>
+
+        {/* Google Sign-in Button */}
+        <GoogleSignInButton />
       </div>
+      <Footer />
     </div>
   );
 };
